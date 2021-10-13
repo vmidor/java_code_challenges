697ef1c59dd156d4692a1bd69b0a4d154da9e763
vmidor <76964172+vmidor@users.noreply.github.com>
2021-10-12 20:57:09 -0400

    solve the problem start

--- test.java
+++ test.java
@@ -1,31 +1,16 @@
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Scanner;
 
-import org.junit.jupiter.api.Test;
+public class EvenOrOdd {
 
-public class EvenOrOddTest {
-
-    @Test
-    public void isEven_true() {
-        assertTrue(EvenOrOdd.isEven(0));
-        assertTrue(EvenOrOdd.isEven(2));
-        assertTrue(EvenOrOdd.isEven(4));
-        assertTrue(EvenOrOdd.isEven(6));
-        assertTrue(EvenOrOdd.isEven(8));
-        assertTrue(EvenOrOdd.isEven(-20));
-        assertTrue(EvenOrOdd.isEven(100));
-        assertTrue(EvenOrOdd.isEven(-46));
+    public static boolean isEven(int n) {
+        return true;
     }
 
-    @Test
-    public void isEven_false() {
-        assertFalse(EvenOrOdd.isEven(1));
-        assertFalse(EvenOrOdd.isEven(3));
-        assertFalse(EvenOrOdd.isEven(5));
-        assertFalse(EvenOrOdd.isEven(7));
-        assertFalse(EvenOrOdd.isEven(9));
-        assertFalse(EvenOrOdd.isEven(-11));
-        assertFalse(EvenOrOdd.isEven(93));
-        assertFalse(EvenOrOdd.isEven(-75));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int userNum = scanner.nextInt();
+        System.out.println("Is the number even? " + isEven(userNum));
     }
 }
